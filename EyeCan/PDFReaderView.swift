4c8d1f4bfde10c25d95c0347ed0d39aab98706fa
Litvak Lev <sn.delui@gmail.com>
2023-02-01 11:59:06 +0500

    Add prototype of 'PDFReaderView'

---
+++ EyeCan/PDFReaderView.swift
@@ -0,0 +1,63 @@
+//
+//  PDFReaderView.swift
+//  EyeCan
+//
+//  Created by Lev Litvak on 01.02.2023.
+//
+
+import SwiftUI
+
+struct PDFReaderView: View {
+    let text: String
+    let onFileHasBeenChosen: (Result<URL, Error>) -> Void
+
+    @State private var showChooseFileDialog = false
+    
+    var body: some View {
+        VStack {
+            title
+            chooseFileButton
+            fileContent
+        }
+        .padding(.horizontal)
+        .fileImporter(
+            isPresented: $showChooseFileDialog,
+            allowedContentTypes: [.pdf],
+            onCompletion: onFileHasBeenChosen
+        )
+    }
+    
+    var title: some View {
+        Text("PDF Reader")
+            .font(.largeTitle)
+    }
+    
+    var chooseFileButton: some View {
+        Button {
+            showChooseFileDialog = true
+        } label: {
+            Text("Choose PDF file")
+        }
+        .buttonStyle(.bordered)
+    }
+    
+    var fileContent: some View {
+        ScrollView {
+            Text(text)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .multilineTextAlignment(.leading)
+            
+        }
+        .background {
+            RoundedRectangle(cornerRadius: 10)
+                .strokeBorder()
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct PDFReaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        PDFReaderView(text: "", onFileHasBeenChosen: { _ in })
+    }
+}

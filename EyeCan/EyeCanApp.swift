68f344957d1d78e861e24bbb317e0747fb12d6f1
Litvak Lev <sn.delui@gmail.com>
2023-02-01 11:58:14 +0500

    Initial Commit

---
+++ EyeCan/EyeCanApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeCanApp.swift
+//  EyeCan
+//
+//  Created by Lev Litvak on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct EyeCanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

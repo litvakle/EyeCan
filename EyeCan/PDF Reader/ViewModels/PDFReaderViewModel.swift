cd7ce1178dbcba2a84382e615932cb8a7825db13
Litvak Lev <sn.delui@gmail.com>
2023-02-01 13:00:25 +0500

    Add 'PDFReaderViewModel' prototype

---
+++ EyeCan/PDF Reader/ViewModels/PDFReaderViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  PDFReaderViewModel.swift
+//  EyeCan
+//
+//  Created by Lev Litvak on 01.02.2023.
+//
+
+import Combine
+import PDFKit
+
+class PDFReaderViewModel: ObservableObject {
+    @Published private(set) var text = ""
+    @Published private(set) var error: PDFReaderError?
+    
+    let reader: PDFReader
+    
+    init(reader: PDFReader) {
+        self.reader = reader
+    }
+    
+    enum PDFReaderError {
+        case chooseDialog(error: String)
+        case pdfReader(error: String)
+    }
+    
+    func readFile(from result: Result<URL, Error>) {
+        
+    }
+}
+
